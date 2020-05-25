a2e30b45c64ed9ca90dd0cf5bee562a7daa85344
Protein phase transitions in health and disease <65223756+BEBlab@users.noreply.github.com>
2020-05-25 18:09:15 +0200

    Add files via upload

---
+++ R/01. centering and FDR correction.R
@@ -0,0 +1,255 @@
+require(readxl)
+require(ggplot2)
+require(ggpubr)
+require(hexbin)
+
+
+dir.create("Centering and FDR correction")
+path="Centering and FDR correction"
+
+
+#required data:
+#excel file MS_BL_BB_processed_data.xlsx
+
+#import data
+silent<-read_excel("MS_BL_BB_processed_data.xlsx", sheet="synonymous")
+singles<-read_excel("MS_BL_BB_processed_data.xlsx", sheet="1 aa change")
+doubles<-read_excel("MS_BL_BB_processed_data.xlsx", sheet="2 aa changes")
+
+
+#centering to the weighted mean of synonymous with 1 mut codon
+mean_syn_1codon<-weighted.mean(silent[silent$Nmut_codons==1,]$nscore, silent[silent$Nmut_codons==1,]$sigma^-2, na.rm = T)
+
+###silent
+silent$nscore_c<-as.numeric(paste(silent$nscore+(-mean_syn_1codon)))
+silent$ID<-"silent"
+silent$Mut<-"silent"
+
+
+###singles
+singles$nscore_c<-as.numeric(paste(as.numeric(singles$nscore)+(-mean_syn_1codon)))
+singles$nscore1_c<-as.numeric(paste(as.numeric(singles$nscore1)+(-mean_syn_1codon)))
+singles$nscore2_c<-as.numeric(paste(as.numeric(singles$nscore2)+(-mean_syn_1codon)))
+singles$nscore3_c<-as.numeric(paste(as.numeric(singles$nscore3)+(-mean_syn_1codon)))
+
+singles$ID<-paste(singles$WT_AA, singles$Pos, singles$Mut, sep = "-")
+
+# FDR=0.1 correction and assignment into categories
+
+singles$zscore<-singles$nscore_c/singles$sigma
+singles$p.adjust<-p.adjust(2*pnorm(-abs(singles$zscore)), method = "BH")
+
+singles$sig_10<-FALSE
+singles[singles$p.adjust<0.1,]$sig_10<-TRUE
+
+singles$category_10<-"WT-like"
+singles[singles$sig_10==T & singles$nscore_c<0,]$category_10<-"NS_dec"
+singles[singles$sig_10==T & singles$nscore_c>0,]$category_10<-"NS_inc"
+
+#with stops
+singles_stops<-singles
+#remove stops
+singles<-singles[singles$Mut!="*",]
+
+
+
+### doubles
+doubles$nscore_c<-as.numeric(paste(as.numeric(doubles$nscore)+(-mean_syn_1codon)))
+doubles$nscore1_c<-as.numeric(paste(as.numeric(doubles$nscore1)+(-mean_syn_1codon)))
+doubles$nscore2_c<-as.numeric(paste(as.numeric(doubles$nscore2)+(-mean_syn_1codon)))
+doubles$nscore3_c<-as.numeric(paste(as.numeric(doubles$nscore3)+(-mean_syn_1codon)))
+
+doubles$ID_mut1<-paste(doubles$WT_AA1, doubles$Pos1, doubles$Mut1, sep = "-")
+doubles$ID_mut2<-paste(doubles$WT_AA2, doubles$Pos2, doubles$Mut2, sep = "-")
+doubles$ID<-paste(doubles$ID_mut1, doubles$ID_mut2, sep="_")
+
+
+# FDR=0.1 correction and assignment into categories
+
+doubles$zscore<-doubles$nscore_c/doubles$sigma
+doubles$p.adjust<-p.adjust(2*pnorm(-abs(doubles$zscore)), method = "BH")
+
+
+doubles$sig_10<-FALSE
+doubles[doubles$p.adjust<0.1,]$sig_10<-TRUE
+
+doubles$category_10<-"WT-like"
+doubles[doubles$sig_10==T & doubles$nscore_c<0,]$category_10<-"NS_dec"
+doubles[doubles$sig_10==T & doubles$nscore_c>0,]$category_10<-"NS_inc"
+
+
+#remove stops
+doubles<-doubles[!doubles$Mut1=="*",]
+doubles<-doubles[!doubles$Mut2=="*",]
+
+
+### singles and doubles
+singles_doubles<-singles
+colnames(singles_doubles)[c(1,2,3)]<-c("Pos1", "WT_AA1", "Mut1")
+singles_doubles[,c("Pos2","WT_AA2", "Mut2", "ID_mut1", "ID_mut2")]<-NA
+singles_doubles<-rbind(singles_doubles, doubles)
+
+
+save(silent, singles, singles_stops, singles_doubles, doubles, file="nscore_df.RData")
+
+
+
+
+#### plots ####
+
+# distribution singles (Missense, Nonsense and Synonymous)
+
+dist<-singles_stops[,c("Mut", "nscore_c")]
+dist<-rbind(dist, silent[silent$Nmut_codons==1,c("Mut", "nscore_c")])
+dist$type<-"Missense"
+dist[dist$Mut=="*",]$type<-"Nonsense"
+dist[dist$Mut=="silent",]$type<-"Synonymous"
+
+p_hist<-ggplot(dist, aes(x=nscore_c))+
+  geom_histogram(binwidth = 0.2, position="identity", alpha=0.8, color=NA, aes(fill=factor(type, levels=c("Missense", "Nonsense", "Synonymous"))))+
+  geom_vline(aes(xintercept=0), color="black", linetype="dashed", size=0.5)+
+  theme_bw()+
+ #scale_fill_manual(values=c("grey70", "grey40", "grey2"))+
+  scale_fill_manual(values=c("grey70", "darkblue","#6666b9"))+
+  labs(x="Nucleation score", y="Counts")+
+  theme(legend.position = c(0.3,0.8),
+        legend.text = element_text(size=18),
+        legend.title = element_blank(),
+        legend.key.size = unit(0.5, "cm"),
+        legend.key.width = unit(0.5,"cm"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.line = element_line(color='black'),
+        axis.title  = element_text(size = 25),
+        axis.text = element_text(size=18),
+        strip.text = element_text(size=15),
+        plot.margin = margin(t=1, r=1, b=1, l=1, unit = 'cm'))+
+  xlim(-6,3.5)
+p_hist
+
+
+# distribution doubles (don't contain stops)
+
+dist<-doubles
+dist$type<-"Missense"
+
+p_hist_doubles<-ggplot(dist, aes(x=nscore_c) )+
+  geom_histogram(binwidth = 0.2, position="identity", alpha=0.8, color=NA, aes(fill=factor(type, levels=c("Missense"))))+
+  geom_vline(aes(xintercept=0), color="black", linetype="dashed", size=0.5)+
+  theme_bw()+
+  scale_fill_manual(values="grey70")+
+  labs(x="Nucleation score", y="Counts")+
+  theme(legend.position = c(0.9,0.8),
+        legend.text = element_text(size=18),
+        legend.title = element_blank(),
+        legend.key.size = unit(0.5, "cm"),
+        legend.key.width = unit(0.5,"cm"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.line = element_line(color='black'),
+        axis.title  = element_text(size = 25),
+        axis.text = element_text(size=18),
+        strip.text = element_text(size=15),
+        plot.margin = margin(t=1, r=1, b=1, l=1, unit = 'cm'))+
+  xlim(c(-6,3.5))
+
+p_hist_doubles
+
+#arragne singles and doubles
+arr<-ggarrange(p_hist, p_hist_doubles, ncol=1, align = "v")
+ggsave(arr, path=path, file="p_hist_singles_doubles.pdf",width=6, height=8, useDingbats=FALSE)
+
+
+
+# correlation between replicates
+
+#1 and 2
+subset<-singles_doubles[!is.na(singles_doubles$nscore1_c),]
+subset<-subset[!is.na(subset$nscore2_c),]
+n<-length(subset$ID)
+
+corr<-cor.test(singles_doubles$nscore1_c, singles_doubles$nscore2_c, use="complete.obs")
+R<-corr$estimate
+p<-corr$p.value
+
+p_corr<-ggplot(singles_doubles, aes(x=nscore1_c, y=nscore2_c) )+
+  stat_binhex()+
+  theme_bw()+
+  labs(x="Replicate 1", y="Replicate 2")+
+  theme(  panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          axis.line = element_line(color='black'),
+          axis.title  = element_text(size = 25),
+          axis.text = element_text(size=18))+
+  annotate("text", x = -5, y = 3, label = paste0("R=", round(R, 2)), size=8)+
+  annotate("text", x = -5, y = 2, label = paste0("p=",format(p, digits = 2, scientific = T)), size=8)+
+
+  scale_fill_gradient(high="grey30", low="grey90")+
+  geom_smooth(method = "lm", linetype = 2, size=1, se = F, color = "black")
+p_corr
+
+
+ggsave(p_corr,path=path, file="p_corr reps 1 and 2.pdf",width=5, height=4, useDingbats=FALSE)
+
+#1 and 3
+subset<-singles_doubles[!is.na(singles_doubles$nscore1_c),]
+subset<-subset[!is.na(subset$nscore3_c),]
+n<-length(subset$ID)
+
+corr<-cor.test(singles_doubles$nscore1_c, singles_doubles$nscore3_c, use="complete.obs")
+R<-corr$estimate
+p<-corr$p.value
+
+p_corr<-ggplot(singles_doubles, aes(x=nscore1_c, y=nscore3_c) )+
+  stat_binhex()+
+  theme_bw()+
+  labs(x="Replicate 1", y="Replicate 3")+
+  theme(  panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          axis.line = element_line(color='black'),
+          axis.title  = element_text(size = 25),
+          axis.text = element_text(size=18))+
+  annotate("text", x = -5, y = 3, label = paste0("R=", round(R, 2)), size=8)+
+  annotate("text", x = -5, y = 2, label = paste0("p=",format(p, digits = 2, scientific = T)), size=8)+
+  
+  scale_fill_gradient(high="grey30", low="grey90")+
+  geom_smooth(method = "lm", linetype = 2, size=1, se = F, color = "black")
+p_corr
+
+
+ggsave(p_corr, path=path, file="p_corr reps 1 and 3.pdf",width=5, height=4, useDingbats=FALSE)
+
+
+#2 and 3
+subset<-singles_doubles[!is.na(singles_doubles$nscore2_c),]
+subset<-subset[!is.na(subset$nscore3_c),]
+n<-length(subset$ID)
+
+corr<-cor.test(singles_doubles$nscore2_c, singles_doubles$nscore3_c, use="complete.obs")
+R<-corr$estimate
+p<-corr$p.value
+
+p_corr<-ggplot(singles_doubles, aes(x=nscore2_c, y=nscore3_c) )+
+  stat_binhex()+
+  theme_bw()+
+  labs(x="Replicate 2", y="Replicate 3")+
+  theme(  panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          axis.line = element_line(color='black'),
+          axis.title  = element_text(size = 25),
+          axis.text = element_text(size=18))+
+  annotate("text", x = -5, y = 3, label = paste0("R=", round(R, 2)), size=8)+
+  annotate("text", x = -5, y = 2, label = paste0("p=",format(p, digits = 2, scientific = T)), size=8)+
+  
+  scale_fill_gradient(high="grey30", low="grey90")+
+  geom_smooth(method = "lm", linetype = 2, size=1, se = F, color = "black")
+p_corr
+
+
+ggsave(p_corr,path=path, file="p_corr reps 2 and 3.pdf",width=5, height=4, useDingbats=FALSE)
+
---
+++ R/03. determinants of nucleation.R
@@ -0,0 +1,318 @@
+require(ggplot2)
+require(reshape2)
+require(dplyr)
+
+dir.create("Determinants of A-beta nucleation")
+path="Determinants of A-beta nucleation"
+
+#required data:
+load("nscore_df.RData")
+
+
+AA_type<-data.frame("AA"= c("G","A","V","L","M","I","F","Y","W","K","R","D","E","S","T","C","N","Q","H", "P" ),
+                   "name_AA"=c("Glycine", "Alanine","Valine","Leucine","Methionine","Isoleucine","Phenylalanine",
+                   "Tyrosine","Tryptophan","Lysine","Arginine","Aspartic acid","Glutamic acid","Serine","Threonine",
+                   "Cysteine","Asparagine","Glutamine","Histidine","Proline"),
+                    "type"=c("glycine",rep("aliphatic",5),rep("aromatic",3),rep("positive",2), "polar",rep("negative",2),rep("polar",5),"proline"))
+ABseq=c("D1","A2","E3","F4","R5","H6","D7","S8","G9","Y10","E11","V12","H13","H14","Q15","K16","L17","V18","F19","F20","A21","E22","D23","V24","G25","S26","N27","K28","G29","A30","I31","I32","G33","L34","M35","V36","G37","G38","V39","V40","I41","A42")
+
+labels_ABseq=c("D\n1","A\n2","E\n3","F\n4","R\n5","H\n6","D\n7","S\n8","G\n9","Y\n10",
+               "E\n11","V\n12","H\n13","H\n14","Q\n15","K\n16","L\n17","V\n18","F\n19","F\n20",
+               "A\n21","E\n22","D\n23","V\n24","G\n25","S\n26","N\n27","K\n28","G\n29","A\n30",
+               "I\n31","I\n32","G\n33","L\n34","M\n35","V\n36","G\n37","G\n38","V\n39","V\n40","I\n41","A\n42")
+levels=c("aliphatic", "aromatic", "negative", "positive", "polar", "histidine", "glycine", "proline")
+
+disease_mutations<- c("H-6-R", "D-7-N", "D-7-H", "E-11-K","K-16-N", "A-21-G", "E-22-Q", "E-22-K", "E-22-G", "D-23-N", "L-34-V", "A-42-T")
+
+color=c("aliphatic"="darkgrey",
+        "aromatic"="#9A703EFF",
+        "negative"="#EE0011FF",
+        "positive"="#0C5BB0FF", 
+        "polar"="#15983DFF", 
+        "glycine"="#FA6B09FF", 
+        "proline"="#FEC10BFF")
+
+color_axis=c("D1"="#EE0011FF","A2"="darkgrey","E3"="#EE0011FF","F4"="#9A703EFF","R5"="#0C5BB0FF","H6"="#15983DFF",
+             "D7"="#EE0011FF","S8"="#15983DFF","G9"="#FA6B09FF","Y10"="#9A703EFF","E11"="#EE0011FF","V12"="darkgrey",
+             "H13"="#15983DFF","H14"="#15983DFF","Q15"="#15983DFF","K16"="#0C5BB0FF","L17"="darkgrey","V18"="darkgrey",
+             "F19"="#9A703EFF","F20"="#9A703EFF","A21"="darkgrey","E22"="#EE0011FF","D23"="#EE0011FF","V24"="darkgrey",
+             "G25"="#FA6B09FF","S26"="#15983DFF","N27"="#15983DFF","K28"="#0C5BB0FF","G29"="#FA6B09FF","A30"="darkgrey",
+             "I31"="darkgrey","I32"="darkgrey","G33"="#FA6B09FF","L34"="darkgrey","M35"="darkgrey","V36"="darkgrey",
+             "G37"="#FA6B09FF","G38"="#FA6B09FF","V39"="darkgrey","V40"="darkgrey","I41"="darkgrey","A42"="darkgrey")
+
+
+
+# add info of AA type for WT and Mut;  AA WT+position; and fAD info
+singles$WT_type<-NA
+singles$Mut_type<-NA
+
+for(i in 1:nrow(singles)){
+  singles[i,]$WT_type<-as.character(AA_type[AA_type$AA==singles[i,]$WT_AA,]$type)
+  singles[i,]$Mut_type<-as.character(AA_type[AA_type$AA==singles[i,]$Mut,]$type)
+}
+
+
+singles$names<-paste0(singles$WT_AA, singles$Pos)
+
+
+singles$fAD<-"non-fAD"
+singles[singles$ID %in% disease_mutations,]$fAD<-"fAD"
+singles[singles$ID=="A-2-V",]$fAD<-"recessive"
+
+
+
+
+
+
+
+
+
+### master boxplot
+
+p_boxplot<-ggplot(singles, aes(x = factor(names, levels=ABseq, labels=labels_ABseq), y = nscore_c)) +
+  geom_hline(yintercept = 0, size=0.5, colour="black", show.legend = F)+
+  geom_boxplot(outlier.shape = NA, color="grey", fill="white", size=0.3) +
+  
+  geom_point(data=singles, aes(y=0, color=factor(WT_type, levels=levels),size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1"))),alpha=0.8, shape=8, size=2.5)+ 
+  geom_jitter(data=singles, aes(color=factor(Mut_type, levels=levels),size=factor(sig_10,levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)), width=0.2, alpha=0.8)+
+  
+  scale_color_manual(values=color)+
+  scale_size_manual(values=c(2,4))+
+  scale_shape_manual(values=c(17,16,18))+
+  
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.line.y = element_line(color="black"),
+        axis.text.x = element_text(color=color_axis, size=12),
+        axis.title.y = element_text(size=15))+
+  guides(fill=F)+
+  labs(y="Nucleation score",x="AB(1-42) WT amino acid and position",shape="fAD category", size="FDR", color="AA type")
+p_boxplot
+
+
+ggsave(p_boxplot,path=path, file="master_boxplot.pdf", width = 15, height = 6)
+
+
+
+
+
+### master boxplot with only Proline and Threonine
+
+singles$outline<-"other"
+singles[singles$Mut=="P",]$outline<-"proline"
+singles[singles$Mut=="T",]$outline<-"threonine"
+
+
+p_boxplot<-ggplot(singles, aes(x = factor(names, levels=ABseq, labels=labels_ABseq), y = nscore_c)) +
+  geom_hline(yintercept = 0, size=0.5, colour="black", show.legend = F)+
+  geom_boxplot( size=0.2, outlier.shape = NA, color="grey90", fill="white") +
+  
+  geom_jitter(data=singles[singles$outline=="other",],
+              aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),
+              width=0.25, alpha=1, stroke=1, color="grey90", fill="grey90")+
+  
+  geom_jitter(data=singles[singles$outline %in% "proline",],
+              aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),
+              color="grey30",fill="grey30", width=0.25, alpha=1, stroke=0.1)+
+  geom_jitter(data=singles[singles$outline %in% "threonine",],
+              aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),
+              color="grey30", fill="white", width=0.25, alpha=1, stroke=1)+
+  
+
+  scale_color_manual(values=c("#A1C720FF", "grey30" ))+
+  scale_size_manual(values=c(1.5,3))+
+  
+  scale_shape_manual(values=c(24,21,23))+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.line.y = element_line(color="black"),
+        axis.text.x = element_text(color="black", size=12),
+        axis.title.y = element_text(size=15))+
+  labs(y="Nucleation score",x="AB(1-42) WT amino acid and position",shape="fAD category", size="FDR")
+p_boxplot
+
+
+ggsave(p_boxplot,path=path, file="boxplot_PT.pdf", width = 12, height = 4)
+
+
+
+
+
+
+
+### master boxplot with only Isoleucine and Valine
+
+singles$outline<-"other"
+singles[singles$Mut=="V",]$outline<-"valine"
+singles[singles$Mut=="I",]$outline<-"isoleucine"
+
+
+p_boxplot<-ggplot(singles, aes(x = factor(names, levels=ABseq, labels=labels_ABseq), y = nscore_c)) +
+  geom_hline(yintercept = 0, size=0.5, colour="black", show.legend = F)+
+  geom_boxplot(size=0.2, outlier.shape = NA, color="grey90", fill="white") +
+ 
+  geom_jitter(data=singles[singles$outline=="other",],
+              aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),
+              width=0.25, alpha=1, stroke=1, color="grey90", fill="grey90")+
+  
+  geom_jitter(data=singles[singles$outline %in% "valine",],
+              aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),
+              color="grey30",fill="grey30", width=0.25, alpha=1, stroke=0.1)+
+  geom_jitter(data=singles[singles$outline %in% "isoleucine",],
+              aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),
+              color="grey30", fill="white",  width=0.25, alpha=1, stroke=1)+
+  
+  geom_point(data=singles[singles$WT_AA=="V",], 
+             color="grey30",     aes(y=0),  alpha=0.8, shape=8, size=2.5)+
+  geom_point(data=singles[singles$WT_AA=="I",], 
+             color="grey30",     aes(y=0),  alpha=0.8, shape=4, size=2.5)+
+  
+  scale_color_manual(values=c("#EE0011FF", "#0C5BB0FF" ))+
+  scale_size_manual(values=c(1.5,3))+
+  scale_shape_manual(values=c(24,21,23))+
+  
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.line.y = element_line(color="black"),
+        axis.text.x = element_text(color="black", size=12),
+        axis.title.y = element_text(size=15))+
+ 
+  labs(y="Nucleation score",x="AB(1-42) WT amino acid and position",shape="fAD category", size="FDR")
+p_boxplot
+
+ggsave(p_boxplot,path=path, file="boxplot_IV.pdf", width = 12, height = 4)
+
+
+
+
+
+
+
+
+### master boxplot for each AA
+
+
+for (AA in unique(singles$Mut)){
+  if(length(which(singles$WT_AA==AA))==0){
+    
+
+p_AA<-ggplot(singles, aes(x = factor(names, levels=ABseq, labels=labels_ABseq), y = nscore_c)) +
+      
+      geom_hline(aes(yintercept = 0), size=0.15, colour="black")+
+      geom_boxplot( size=0.2, outlier.shape = NA, color="grey90", fill="white") +
+      
+      geom_jitter(data=singles[singles$Mut!=AA,],
+                  aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),
+                  width=0.2, alpha=1, stroke=1, color="grey90", fill="grey90")+
+      
+      
+      geom_jitter(data=singles[singles$Mut==AA,], 
+                  aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),
+                  color="grey30",fill="grey30", width=0.2, alpha=1, stroke=0.1)+
+      
+      scale_size_manual(values=c(2.5,4))+
+      scale_shape_manual(values=c(17,16, 18))+
+      
+      theme_bw()+
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.border = element_blank(),
+            plot.title=element_text(size=25),
+            axis.line.y = element_line(color="black", size=0.15),
+            axis.text.x = element_text(color="black", size=14),
+            axis.text.y = element_text(size=14))+
+      labs(title=AA_type[AA_type$AA==AA,]$name_AA, shape="fAD class", size="FDR",y="Nucleation score",x="AB(1-42) WT amino acid and position")
+    
+    ggsave(p_AA, path=path, file=paste0(AA, "_AA_boxplot.pdf"), width = 12, height = 4)
+    
+    
+  }else{
+    
+    
+    p_AA<-ggplot(singles, aes(x = factor(names, levels=ABseq, labels=labels_ABseq), y = nscore_c)) +
+      
+      geom_hline(aes(yintercept = 0), size=0.15, colour="black", show.legend = F)+
+      geom_boxplot( size=0.2, outlier.shape = NA, color="grey90", fill="white") +
+      
+      geom_jitter(data=singles[singles$Mut!=AA,],
+                  aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),  
+                  width=0.2, alpha=1, stroke=1, color="grey90", fill="grey90")+
+      
+      geom_point(data=singles[singles$WT_AA==AA,],  
+                 color="grey30",     aes(y=0),  alpha=1, shape=8, size=3)+
+      
+      geom_jitter(data=singles[singles$Mut==AA,],    
+                  aes(size=factor(sig_10, levels=c(F,T), labels=c("FDR>0.1", "FDR<0.1")), shape=factor(fAD)),
+                  color="grey30",fill="grey30",  width=0.2, alpha=1, stroke=0.1)+
+      
+      
+      scale_size_manual(values=c(2.5,4))+
+      scale_shape_manual(values=c(17,16, 18))+
+      
+      theme_bw()+
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.border = element_blank(),
+            plot.title=element_text(size=25),
+            axis.line.y = element_line(color="black", size=0.15),
+            axis.text.x = element_text(color="black", size=14),
+            axis.text.y = element_text(size=14)
+            )+
+      labs(title=AA_type[AA_type$AA==AA,]$name_AA, shape="fAD class", size="FDR", y="Nucleation score",x="AB(1-42) WT amino acid and position")
+    
+    
+    ggsave(p_AA,path=path, file=paste0(AA, "_AA_boxplot.pdf"), width = 12, height = 4)
+  }}
+
+
+
+
+
+
+
+
+### boxplots to each Mut AA
+
+list<-list(
+  "Nt (1-26)"=c(2,4:6,8:10,12:16,18:21,24:26),
+  "Ct (27-41)"=c(27:41),
+  "Negatively charged gatekeepers"=c(1,3,7,11,22),
+  "Negatively charged positions"=c(1,3,7,11,22,23)
+)
+
+
+
+for(region in names(list)){
+  
+  residues<-list[[region]]
+  
+  
+p<-ggplot(singles[singles$Pos %in% residues,], aes(x= reorder(Mut, -nscore_c), y = nscore_c, fill=Mut_type)) +
+    theme_bw()+
+    geom_hline(aes(yintercept = 0), size=0.2, colour="black", show.legend = F)+
+    geom_boxplot( width=0.5, size=0.2,outlier.shape = NA, alpha=0.8) +
+    theme(axis.text.x = element_text( size=15, hjust=0.5), 
+          axis.title.y = element_text(size=10),
+          title = element_text(size=15),
+          axis.line.y = element_line(size=0.2),
+          panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())+
+    labs(y="Nucleation score", title=paste0(region), fill="Mutant AA type", x="Mutant amino acid")+
+    scale_fill_manual(values=color)+
+    geom_jitter( position = position_jitter(width = 0.2), size=1, show.legend = T, alpha=0.8, shape=16)
+  
+p
+
+ggsave(p,path = path, file=paste0(region, "_to_MutAA_boxplot.pdf"), width = 8, height = 3)
+
+
+  
+}
+
